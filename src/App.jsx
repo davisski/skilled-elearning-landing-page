e821c596c2cca325e267aca55a6bbf4d867f3b93
Davis Skipsna <skipsna.davis@gmail.com>
2026-03-16 23:25:34 +0200

    #3 - button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import ButtonComponent from './components/ButtonComponent'
 function App() {
 
   return (
     <>
-      <section>
-      section    
+      <section className='flex py-4 flex-col gap-1.5'>
+        <ButtonComponent theme="orange" title="Get Started" />
+        <ButtonComponent theme="blue" title="Get Started" />
+        <ButtonComponent theme="purple" title="Get Started" />
       </section>
     </>
   )
